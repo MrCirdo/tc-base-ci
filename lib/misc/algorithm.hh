322b3c23cf643d297727fd1d123607b322394eb9
Etienne Renault <renault@lrde.epita.fr>
2020-02-09 08:40:45 +0000

    2022-tc-base-1.0.

---
+++ lib/misc/algorithm.hh
@@ -0,0 +1,31 @@
+/**
+ ** \file misc/algorithm.hh
+ ** \brief Some syntactic sugar to look for things in STL containers.
+ */
+
+#pragma once
+
+#include <iosfwd>
+
+namespace misc
+{
+  /// Invoke delete on all the members of \a c, and clear it.
+  template <typename Container> void deep_clear(Container& c);
+
+  /// Apply \a f to all the members of \a c, and return it.
+  template <typename Container, typename Functor>
+  inline Functor& for_each(Container& c, Functor& v);
+
+  /// Is \a v member of \a c?
+  template <typename Container>
+  inline bool has(const Container& c, const typename Container::value_type& v);
+
+  /// Insert or update \a key -> \a value in \a map, return iterator to it.
+  template <typename Map>
+  typename Map::iterator put(Map& map,
+                             const typename Map::key_type& key,
+                             const typename Map::mapped_type& value);
+
+} // namespace misc
+
+#include <misc/algorithm.hxx>
---
+++ src/object/test-type.cc
@@ -0,0 +1,17 @@
+/// Test the typing with object extensions.
+
+#include <cstdlib>
+#include <iostream>
+
+#include <ast/decs-list.hh>
+#include <ast/exp.hh>
+#include <ast/libast.hh>
+#include <object/libobject.hh>
+#include <parse/libparse.hh>
+
+const char* program_name = "test-type";
+
+
+int main()
+{
+}
