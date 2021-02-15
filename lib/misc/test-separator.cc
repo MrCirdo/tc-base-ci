fbec1be63bf3407c1bdae024f3229f072a7e9409
Etienne Renault <renault@lrde.epita.fr>
2021-02-15 09:03:03 +0000

    2023-tc-base-1.0.

---
+++ lib/misc/test-separator.cc
@@ -0,0 +1,43 @@
+/**
+ ** Test code for misc/separator.hh features.
+ */
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#include <misc/contract.hh>
+#include <misc/pair.hh>
+#include <misc/separator.hh>
+#include <range/v3/algorithm/fill.hpp>
+
+int main()
+{
+  {
+    std::vector<int> v(4);
+    ranges::fill(v, 51);
+
+    std::ostringstream s;
+    s << misc::separate(v, ", ");
+    assertion(s.str() == "51, 51, 51, 51");
+  }
+
+  {
+    std::vector<int> v(4);
+    ranges::fill(v, 51);
+
+    std::ostringstream s;
+    s << misc::separate(v, misc::make_pair(",", " "));
+    assertion(s.str() == "51, 51, 51, 51");
+  }
+
+  {
+    int p = 51;
+    std::vector<int*> v(4);
+    ranges::fill(v, &p);
+
+    std::ostringstream s;
+    s << misc::separate(v, ", ");
+    assertion(s.str() == "51, 51, 51, 51");
+  }
+}
