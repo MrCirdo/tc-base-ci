f768855222b9a3f29305b40e90faa9de7e183601
Odric Roux-Paris <odric.roux-paris@epita.fr>
2021-02-17 00:45:02 +0100

    test: Add more test for misc::unique class.

--- lib/misc/test-unique.cc
+++ lib/misc/test-unique.cc
@@ -3,11 +3,11 @@
  */
 
 #include <iostream>
+#include <string>
 
 #include <misc/contract.hh>
 #include <misc/unique.hh>
-
-int main()
+void test_with_int()
 {
   using unique_int = misc::unique<int>;
   unique_int the_answer = 42;
@@ -20,4 +20,25 @@ int main()
   assertion(the_answer != the_solution);
 
   std::cout << the_answer << '\n';
+}
+
+void test_with_string()
+{
+  using unique_str = misc::unique<std::string>;
+  unique_str the_answer = std::string{"coucou"};
+  unique_str the_same_answer =  std::string{"coucou"};
+  unique_str the_solution = std::string{"coucou mais un peu différent"};
+
+  // Checking misc::unique<int>.
+  assertion(the_answer == unique_str(std::string{"coucou"}));
+  assertion(the_answer == the_same_answer);
+  assertion(the_answer != the_solution);
+
+  std::cout << the_answer << '\n';
+}
+
+int main()
+{
+  test_with_string();
+  test_with_int();
 }
